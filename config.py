32517c15a9765696e589d0311228ae31411cc3df
itsaLca <6356217+itsaLca@users.noreply.github.com>
2022-08-12 21:02:04 +0200

    Create config.py
    forgot config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+API_KEY = ''
+API_SECRET = ''
+
+WEBHOOK_PASSPHRASE = '123'
+DISCORD_WEBHOOK_URL = '' # use a string containing your discord webhook url to enable, like: https://discord.com/api/webhooks/100...
