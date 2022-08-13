a5882ed171605e05565d62818cd0dc9ae5d01eb2
itsaLca <6356217+itsaLca@users.noreply.github.com>
2022-08-13 21:03:32 +0200

    polish up, telegram added

--- config.py
+++ config.py
@@ -1,5 +1,20 @@
+# Exchange keys
 API_KEY = ''
 API_SECRET = ''
 
+
+# Webhook Password
 WEBHOOK_PASSPHRASE = '123'
+
+
+# Discord Alert Webhook
+DISCORD_ENABLED = True
 DISCORD_WEBHOOK_URL = '' # use a string containing your discord webhook url to enable, like: https://discord.com/api/webhooks/100...
+
+
+# Telegram Alert Message
+TELEGRAM_ENABLED = True
+TELEGRAM_TOKEN   = '' # Bot token. Get it from @Botfather
+TELEGRAM_CHANNEL = '-100'   # Channel ID (ex. -100123456789) aLca :: Note: -100 is important as start
+TELEGRAM_SESSION_NAME = 'phemex-webhoe' # Session name. Only one session (with a unique session name) can run at a time
+TELEGRAM_OUTPUT_CHANNEL = 'ZeroDayTrading'
