897f27fd9c32c8cf843e4ae223a08a51ed7cb2e7
itsaLca <6356217+itsaLca@users.noreply.github.com>
2022-08-24 01:36:54 +0200

    Delete dydx3 directory

--- dydx3/eth_signing/__init__.py
+++
@@ -1,4 +0,0 @@
-from dydx3.eth_signing.eth_prive_action import SignEthPrivateAction
-from dydx3.eth_signing.onboarding_action import SignOnboardingAction
-from dydx3.eth_signing.signers import SignWithKey
-from dydx3.eth_signing.signers import SignWithWeb3
